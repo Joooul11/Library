be30dcb2792cc1095ee5b99901d69408d9d25ff8
Joooul11 <pavel.sinyagovskiy@gmail.com>
2025-01-01 15:18:14 -0700

    implement book form functionality and styling

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const mylibrary = []
 function Book(title, author, pages, read) {
   this.title = title
-  this.author = author
+  this.author = title
   this.pages = pages
   this.read = read
 }
@@ -43,5 +43,26 @@ function displayBooks() {
         libraryContainer.appendChild(bookCard);
     });
 }
- 
+
+const form = document.getElementById('book-form');
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const title = document.getElementById('title').value;
+  const author = document.getElementById('author').value;
+  const pages = document.getElementById('pages').value;
+  const read = document.getElementById('read').checked;
+  
+  const newBook = new Book(title, author, pages, read);
+  addBookToLibrary(newBook);
+  displayBooks();
+  form.reset();
+});
+
+const newBookBtn = document.createElement('button');
+newBookBtn.textContent = 'New Book';
+newBookBtn.addEventListener('click', () => {
+  form.classList.toggle('hidden');
+});
+document.body.prepend(newBookBtn);
+
 
